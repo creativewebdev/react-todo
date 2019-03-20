907abb3f8d3f9e67cb77301640a31ac460765581
Brandon_J_Allison <you@example.com>
2019-03-20 15:20:53 -0400

    built my todform form wireframe

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Todo = () =>{
+    return(
+<div>
 
-
-    
+</div>
+    );
 }
 
 
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const TodoForm = () =>{
+const TodoForm = props =>{
+return (
+<div>
+<form>
+<input type=""
+        name=""
+        value=""
+        placeholder="need to do?"/>
+
+</form>
+<button>Add to List</button>
+</div>
+);
 
 
+};
 
-}
 
-
-export default ToDoForm;
+export default TodoForm;
